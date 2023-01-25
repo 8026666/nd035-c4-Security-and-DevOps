14d3d65898646f9c01999669f87b61f2a2421190
Hao <8026666@gmail.com>
2023-01-25 14:56:07 -0500

    Fix bugs

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,17 +1,16 @@
 package com.example.demo;
 
-import com.example.demo.controllers.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SareetaApplicationTests {
-	@Autowired
-	private UserController userController;
+
+	@Test
+	public void contextLoads() {
+	}
+
 }
